8614d62412104eb1c0e45b23c9263f321ae09ed1
Matteo Palladino <matteo.palladino@traderepublic.com>
2022-05-11 15:51:03 +0200

    downgrading aws provider to use versions >= 3.74

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   # Create set of S3 bucket ARNs from the names derived from logging attribute objects
   s3_buckets = toset([for o in var.imginfracfg_logging : "arn:aws:s3:::${o.s3_logs.s3_bucket_name}/*"])
 
-  # Create a joined set of component objects from both the managed and created components, derived from their respective input variables 
+  # Create a joined set of component objects from both the managed and created components, derived from their respective input variables
   components = concat([for k, o in var.imgb_components : {
     component_arn = aws_imagebuilder_component.main[k].id
     parameters    = o.parameters != null ? o.parameters : null
@@ -28,6 +28,6 @@ terraform {
   experiments      = [module_variable_optional_attrs]
   required_version = ">= 1.1.0"
   required_providers {
-    aws = ">= 4.9.0"
+    aws = ">= 3.74.0"
   }
 }
